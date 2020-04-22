e281d52afb91b1b7907a592289861533c3b58f20
samll-howe <2678691035@qq.com>
2020-04-22 20:08:25 +0800

    SpringSecurity前后端分离各种情况的返回

--- from-login/src/main/java/com/tangwh/config/SecurityConfig.java
+++ from-login/src/main/java/com/tangwh/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.tangwh.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,6 +10,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.io.PrintWriter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -61,25 +64,73 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .usernameParameter("name")
                 //可以修改 登录时候 password 属性框的name
 //                .passwordParameter("pwd")
-                // 登录成功的跳转地址(服务端跳转) 不管从哪个页面来的 经过登录后 跳转到/s的页面
+                // 登录成功的跳转地址(服务端跳转)      -----前后端不分离  不管从哪个页面来的 经过登录后 跳转到/s的页面
 //                .successForwardUrl("/s")
-                // 登录成功的跳转地址(客户端跳转重定向)
-                .defaultSuccessUrl("/s")
-                //登录失败的跳转 服务端跳转
+                // 登录成功的跳转地址(客户端跳转重定向) -----前后端不分离
+//                .defaultSuccessUrl("/s")
+                // 登录成功后 的函数回调              ----前后端分离
+                .successHandler((req, resp, authentication) -> {
+
+                    //req -- 服务端跳转
+                    //resp-- 客户端挑战 返回JSON
+                    //authentication-- 当前登录成功的用户信息
+                    resp.setContentType("application/json;charset=utf-8");
+                    // 返回JSON 返回当前用户的信息
+                    PrintWriter out = resp.getWriter();
+                    out.write(new ObjectMapper().writeValueAsString(authentication.getPrincipal()));
+
+                    out.flush();
+                    out.close();
+
+
+                })
+                //登录失败的跳转 服务端跳转  -- 前后端不分离
 //                .failureForwardUrl("/s")
-                //登录失败的跳转 客户端跳转(重定向)
+                //登录失败的跳转 客户端跳转(重定向) -- 前后端不分离
 //                .failureUrl("/s")
-                //跟登录相关的页面 都放心
+                //登录失败的 回调  ---前后端分离
+                .failureHandler((req, resp, exception) -> {
+
+                    //req -- 服务端跳转
+                    //resp-- 客户端挑战 返回JSON
+                    //authentication-- 当前登录成功的用户信息
+                    resp.setContentType("application/json;charset=utf-8");
+                    // 返回JSON 返回当前用户的信息
+                    PrintWriter out = resp.getWriter();
+                    out.write(new ObjectMapper().writeValueAsString(exception.getMessage()));
+
+                    out.flush();
+                    out.close();
+
+
+                })
+                //跟登录相关的页面 都放行
                 .permitAll()
                 .and()
                 //注销登录
                 .logout()
                 //注销的地址  GET 请求
                 .logoutUrl("/logout")
-                // 注销地址 Post请求
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "POST"))
-                // 注销成功去哪里?
-                .logoutSuccessUrl("/login.html")
+                // 注销地址 Post请求 -- 前后端不分离
+//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "POST"))
+                // 注销成功去哪里?    -- 前后端不分离
+//                .logoutSuccessUrl("/login.html")
+                .logoutSuccessHandler((req,resp,authentication)->{
+
+
+                    //req -- 服务端跳转
+                    //resp-- 客户端挑战 返回JSON
+                    //authentication-- 当前登录成功的用户信息
+                    resp.setContentType("application/json;charset=utf-8");
+                    // 返回JSON 返回当前用户的信息
+                    PrintWriter out = resp.getWriter();
+                    out.write(new ObjectMapper().writeValueAsString("注销登录成功"));
+
+                    out.flush();
+                    out.close();
+
+
+                })
                 // 是否让Session 失效 默认 失效
 //                .invalidateHttpSession(true)
                 // 清除认证信息  默认 清除
@@ -88,7 +139,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 //关闭csrf
-                .csrf().disable();
+                .csrf().disable()
+                //未认证 的请求  -- 前后端分离
+                .exceptionHandling()
+                .authenticationEntryPoint((req,resp,exception)->{
+
+                    //req -- 服务端跳转
+                    //resp-- 客户端挑战 返回JSON
+                    //authentication-- 当前登录成功的用户信息
+                    resp.setContentType("application/json;charset=utf-8");
+                    // 返回JSON 返回当前用户的信息
+                    PrintWriter out = resp.getWriter();
+                    out.write(new ObjectMapper().writeValueAsString("尚未登录,请登录"));
+
+                    out.flush();
+                    out.close();
+                });
 
     }
 
